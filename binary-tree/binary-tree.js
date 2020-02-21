5a19e25514f54d6a18e277c3033db3f31c6a3897
gortron <gordon.lanza@gmail.com>
2020-02-21 15:25:48 -0800

    added binary tree data structure with add() and maxDepth() functions, initial test for add()

---
+++ binary-tree/binary-tree.js
@@ -0,0 +1,66 @@
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  toObject() {
+    return this.root;
+  }
+
+  add(value) {
+    if (!this.root) {
+      // if there is no root, then set to root
+      this.root = new Node(value);
+      return;
+    }
+
+    // iterative implementation
+    let current = this.root;
+    // loop will zig-zag through the tree, setting current to whichever node we are checking. loop breaks at return, when a new Node is added.
+    while (true) {
+      if (value < current.value) {
+        // go left
+        if (current.left) {
+          current = current.left;
+        } else {
+          current.left = new Node(value);
+          return;
+        }
+      } else {
+        // go right
+        if (current.right) {
+          current = current.right;
+        } else {
+          current.right = new Node(value);
+          return;
+        }
+      }
+    }
+  }
+
+  maxDepth() {
+    if (!this.root) return 0;
+
+    let leftDepth = visit(this.root.left, 1);
+    let rightDepth = visit(this.root.right, 1);
+    return Math.max(leftDepth, rightDepth);
+  }
+
+  visit(node, depth) {
+    if (!node) return depth;
+
+    let left = visit(node.left, depth + 1);
+    let right = visit(node.right, depth + 1);
+    return Math.max(left, right);
+  }
+}
+
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+export { Tree, Node };
---
+++ binary-tree/binary-tree.test.js
@@ -0,0 +1,49 @@
+import { Tree, Node } from "./binary-tree";
+
+// unit tests
+// do not modify the below code
+describe("Binary Tree", function() {
+  let nums = [];
+  let tree = new Tree();
+  let objs = tree.toObject();
+
+  beforeEach(() => {
+    nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
+    tree = new Tree();
+    nums.map(num => tree.add(num));
+    objs = tree.toObject();
+  });
+
+  it("creates a binary tree from an array", () => {
+    expect(objs.value).toEqual(3);
+
+    expect(objs.left.value).toEqual(1);
+    expect(objs.left.left).toBeNull();
+
+    expect(objs.left.right.value).toEqual(2);
+    expect(objs.left.right.left).toBeNull();
+    expect(objs.left.right.right).toBeNull();
+
+    expect(objs.right.value).toEqual(7);
+
+    expect(objs.right.left.value).toEqual(4);
+    expect(objs.right.left.left).toBeNull();
+
+    expect(objs.right.left.right.value).toEqual(6);
+    expect(objs.right.left.right.left.value).toEqual(5);
+    expect(objs.right.left.right.left.right).toBeNull();
+    expect(objs.right.left.right.left.left).toBeNull();
+
+    expect(objs.right.right.value).toEqual(10);
+    expect(objs.right.right.right).toBeNull();
+
+    expect(objs.right.right.left.value).toEqual(9);
+    expect(objs.right.right.left.right).toBeNull();
+
+    expect(objs.right.right.left.left.value).toEqual(8);
+    expect(objs.right.right.left.left.right).toBeNull();
+    expect(objs.right.right.left.left.left).toBeNull();
+  });
+
+  // it("can perform a depth-first search", () => {});
+});
